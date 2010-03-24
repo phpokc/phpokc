435f63b8310112b44f93a3a5be26112215b0c870
Joseph Donley <jdonley83@gmail.com>
2010-03-23 19:22:49 -0500

    files after zend tool

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,27 +2,7 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initAutoload()
-     {
-          // Add autoloader empty namespace
-          $autoLoader = Zend_Loader_Autoloader::getInstance();
-          $autoLoader->registerNamespace('CMS_');
-          $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
-            'basePath'      => APPLICATION_PATH,
-            'namespace'     => '',
-            'resourceTypes' => array(
-                'form' => array(
-                    'path'      => 'forms/',
-                    'namespace' => 'Form_'
-                    ),
-                'model' => array(
-                    'path'      => 'models/',
-                    'namespace' => 'Model_'
-                    )
-               )
-          ));
-          //Return it so that it can be stored by the bootstrap
-          return $autoLoader;
-     }
+
+
 }
 
--- public/index.php
+++ public/index.php
@@ -1,27 +1,26 @@
 <?php
-date_default_timezone_set('America/Chicago');
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-|| define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
-|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-realpath(APPLICATION_PATH . '/../library'),
-get_include_path(),
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
 )));
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+require_once 'Zend/Application.php';  
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
-APPLICATION_ENV,
-APPLICATION_PATH . '/configs/application.ini'
+    APPLICATION_ENV, 
+    APPLICATION_PATH . '/configs/application.ini'
 );
 $application->bootstrap()
-->run();
+            ->run();
