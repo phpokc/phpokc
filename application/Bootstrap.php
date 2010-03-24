3d5abd769090734e4c511ed67dc23585079103a6
Joseph Donley <jdonley83@gmail.com>
2010-03-23 19:36:54 -0500

    added missing files

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,23 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initAutoload()
+     {
+          // Add autoloader empty namespace
+          $autoLoader = Zend_Loader_Autoloader::getInstance();
+          $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+            'basePath'      => APPLICATION_PATH,
+            'namespace'     => '',
+            'resourceTypes' => array(
+                'model' => array(
+                    'path'      => 'models/',
+                    'namespace' => 'Model_'
+                    )
+               )
+          ));
+          //Return it so that it can be stored by the bootstrap
+          return $autoLoader;
+     }
 
 }
 
